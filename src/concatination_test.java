d0b4878f523051cc426d01bdfc95e5b670520c33
Admin <petrodavidenko32@gmail.com>
2022-03-17 01:00:01 +0200

    Initial commit

---
+++ src/concatination_test.java
@@ -0,0 +1,10 @@
+public class concatination_test {
+    public static void main(String[] args) {
+        String morning1 ="доброе";
+        String morning2 ="не доброе, потому-что сегодня понедельник";
+
+        System.out.println("Сегодня утро " + morning1);
+        System.out.println("Сегодня утро " + morning2);
+
+    }
+}
